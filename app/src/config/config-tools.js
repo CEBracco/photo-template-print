64d26d6678070ea627d13f507ea4c6629a1b488d
CEBracco <bracco66.cb@gmail.com>
2019-12-22 16:23:16 -0300

    add electron updater

--- app/src/config/config-tools.js
+++ app/src/config/config-tools.js
@@ -1,6 +1,14 @@
 function configure(basePath) {
+    //autoUpdater
+    const { autoUpdater } = require("electron-updater")
+    autoUpdater.logger = require("electron-log")
+    autoUpdater.logger.transports.file.level = "info"
+    autoUpdater.checkForUpdatesAndNotify()
+
+    //dotEnv
     require('dotenv').config({ path: `${basePath}/.env` })
     
+    //moduleAlias
     const moduleAlias = require('module-alias')
     moduleAlias.addAlias('@appSrc', basePath + "/app/src")
     moduleAlias.addAlias('@localModules', basePath + "/modules")
